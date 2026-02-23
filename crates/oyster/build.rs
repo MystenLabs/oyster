9e97bf05e0fb7c7973a132d05d25ee9f8ea4cc83
Will Bradley <will.bradley@mystenlabs.com>
2026-02-22 22:06:57 -0600

    build: add rerun-if-changed for proto compilation
    Avoids re-running protoc on every incremental build when only .rs files
    change.

--- crates/oyster/build.rs
+++ crates/oyster/build.rs
@@ -1,4 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!("cargo::rerun-if-changed=../pearl/proto/pearl.proto");
     tonic_prost_build::compile_protos("../pearl/proto/pearl.proto")?;
     Ok(())
 }
